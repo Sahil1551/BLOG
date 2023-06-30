ec30fd8f7659c08f6b4bf2c4d422ad20f0de7315
Sahil1551 <106240812+Sahil1551@users.noreply.github.com>
2023-06-30 21:47:30 +0530

    vercel setting done

--- BLOG/BLOG/settings.py
+++ BLOG/BLOG/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v*va(clx6fgy1%x$n9n*f@k&t5$*s6-qxb(y*-hc^kj7%0!w1f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1']
 
 
 # Application definition
@@ -118,10 +118,11 @@ USE_TZ = True
 
 STATIC_URL = '/staticfiles/'
 MEDIA_URL='/img/'
+MEDIA_ROOT='/img'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'staticfiles')
 ]
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
